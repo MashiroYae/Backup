4279d259500775741f91513f604734d9e64625f7
Mashiro <inoriheart@gmail.com>
2022-08-03 09:45:24 +0800

    Wed Aug  3 09:45:24 AM CST 2022

---
+++ ManjaroMainlineKernel.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+RPiVERSION=5.18.12-2
+VERSION=5.18.14-1
+
+cd /Users/mashiro/Library/Caches/mashiro/aarch64/
+rm linux*.pkg.tar.zst
+
+curl -JLO https://mirrors.manjaro.org/repo/arm-unstable/core/aarch64/linux-rpi4-mainline-$RPiVERSION-aarch64.pkg.tar.zst
+#curl -JLO https://mirrors.manjaro.org/repo/arm-unstable/core/aarch64/linux-rpi4-mainline-headers-$VERSION-aarch64.pkg.tar.zst
+
+curl -JLO https://mirrors.manjaro.org/repo/arm-unstable/core/aarch64/linux-$VERSION-aarch64.pkg.tar.zst
