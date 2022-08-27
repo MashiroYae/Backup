ab24c28238f0edc25e6f63512a15e7d70aeb4c0e
Mashiro <inoriheart@gmail.com>
2022-08-27 14:09:30 +0800

    Sat Aug 27 02:09:30 PM CST 2022

--- ManjaroMainlineKernel.sh
+++ ManjaroMainlineKernel.sh
@@ -3,6 +3,7 @@
 RPiVERSION=5.19.3-1
 VERSION=5.19.4-1
 RCVERSION=6.0.rc2-4
+RC=6.0.0rc2-1
 
 cd /Users/mashiro/Library/Caches/mashiro/aarch64/
 
@@ -13,7 +14,6 @@ if [ -f "/Users/mashiro/Library/Caches/mashiro/aarch64/linux-rpi4-mainline-$RPiV
       rm /Users/mashiro/Library/Caches/mashiro/aarch64/linux-rpi4-mainline-*-aarch64.pkg.tar.zst
       curl -JLO https://mirrors.manjaro.org/repo/arm-unstable/core/aarch64/linux-rpi4-mainline-$RPiVERSION-aarch64.pkg.tar.zst
       #curl -JLO https://mirrors.manjaro.org/repo/arm-unstable/core/aarch64/linux-rpi4-mainline-headers-$VERSION-aarch64.pkg.tar.zst
-
 fi
 
 if [ -f "/Users/mashiro/Library/Caches/mashiro/aarch64/linux-$VERSION-aarch64.pkg.tar.zst" ]
@@ -30,4 +30,14 @@ if [ -f "/Users/mashiro/Library/Caches/mashiro/aarch64/linux-rc-$RCVERSION-aarch
   else
       rm /Users/mashiro/Library/Caches/mashiro/aarch64/linux-rc-*-aarch64.pkg.tar.zst
       curl -JLO https://mirrors.manjaro.org/repo/arm-unstable/core/aarch64/linux-rc-$RCVERSION-aarch64.pkg.tar.zst
+fi
+
+
+cd /Users/mashiro/Library/Caches/mashiro/x86_64
+if [ -f "/Users/mashiro/Library/Caches/mashiro/x86_64/linux60-$RC-x86_64.pkg.tar.zst" ]
+  then
+      echo 'NyaNya'
+  else
+      rm /Users/mashiro/Library/Caches/mashiro/x86_64/linux60-*-x86_64.pkg.tar.zst
+      curl -JLO https://mirrors.manjaro.org/repo/unstable/core/x86_64/linux60-$RC-x86_64.pkg.tar.zst
 fi
