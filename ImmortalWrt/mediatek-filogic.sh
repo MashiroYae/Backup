4f39e27c228c016546f3cf7ceb4c4cebd1dd199e
MashiroYae <inoriheart@gmail.com>
2023-11-11 12:28:02 +0800

    Sat Nov 11 12:28:02 PM CST 2023

---
+++ ImmortalWrt/mediatek-filogic.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+cd /srv/http/ImmortalWrt/ImageBuilder || exit
+rm -rf immortalwrt-imagebuilder-mediatek-filogic.Linux-x86_64
+curl -JLO https://downloads.immortalwrt.org/snapshots/targets/mediatek/filogic/immortalwrt-imagebuilder-mediatek-filogic.Linux-x86_64.tar.xz
+tar xf immortalwrt-imagebuilder-mediatek-filogic.Linux-x86_64.tar.xz
+rm immortalwrt-imagebuilder-mediatek-filogic.Linux-x86_64.tar.xz
+cd immortalwrt-imagebuilder-mediatek-filogic.Linux-x86_64 || exit
+
+make image PROFILE=bananapi_bpi-r3-mini PACKAGES="auc blockd bottom btrfs-progs cfdisk dosfstools htop ip-full iw-full kmod-fs-msdos kmod-nvme kmod-usb-net-cdc-subset kmod-usb-storage-extras kmod-usb-storage-uas luci luci-app-attendedsysupgrade luci-ssl-openssl nano-full nvme-cli odhcp6c odhcpd openssh-sftp-server usbutils wget-ssl -automount -bridger -ipv6helper -libustream-openssl -uclient-fetch"
--- OpenWrt/rockchip-armv8.sh
+++ OpenWrt/rockchip-armv8.sh
@@ -7,4 +7,4 @@ tar xf openwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.xz
 rm openwrt-imagebuilder-rockchip-armv8.Linux-x86_64.tar.xz
 cd openwrt-imagebuilder-rockchip-armv8.Linux-x86_64 || exit
 
-make image PROFILE=friendlyarm_nanopi-r4s PACKAGES="auc blockd bottom btrfs-progs cfdisk dnsmasq-full dosfstools fdisk htop ip-full iw-full kmod-fs-msdos kmod-mac80211 kmod-usb-net-cdc-subset kmod-usb-storage-extras luci luci-app-attendedsysupgrade luci-app-uhttpd luci-ssl-openssl nano-full odhcpd openssh-sftp-server usbutils wget-ssl wpad-openssl -dnsmasq -libustream-mbedtls -odhcpd-ipv6only -uclient-fetch"
+make image PROFILE=friendlyarm_nanopi-r4s PACKAGES="auc blockd bottom btrfs-progs cfdisk dnsmasq-full dosfstools fdisk htop ip-full iw-full kmod-fs-msdos kmod-mac80211 kmod-usb-net-cdc-subset kmod-usb-storage-extras luci luci-app-attendedsysupgrade luci-ssl-openssl nano-full odhcpd openssh-sftp-server usbutils wget-ssl wpad-openssl -dnsmasq -libustream-mbedtls -odhcpd-ipv6only -uclient-fetch"
